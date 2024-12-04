224e48becd155e937a874cb0f8fd62fe8b378c3c
madmonl-starkware <liad@starkware.co>
2024-12-04 13:54:05 +0200

    wip

--- apps/starkitchen/src/hooks/useCateringContract.ts
+++ apps/starkitchen/src/hooks/useCateringContract.ts
@@ -1,6 +1,7 @@
-import { CONTRACT_ADDRESS } from "../providers/starknet-provider";
 import CateringAbi from '../providers/catering-abi.json';
 import { useContract } from "@catering-app/starknet-contract-connect";
+
+const CONTRACT_ADDRESS = '0x049c75609bb077a9427bc26a9935472ec75e5508ed216ef7f7ad2693397deebc';
 
 export const useCateringContract = () => {
   return useContract({
