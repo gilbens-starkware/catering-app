3222bbbaf6b5878f8b27d6d8fddd2a51831e8567
Gil Ben-Shachar <gil@starkware.co>
2024-12-11 16:31:58 +0200

    Doc deploy script.
    commit-id:31c1e0c0

--- packages/contracts/event_manager/scripts/deploy_contract.sh
+++ packages/contracts/event_manager/scripts/deploy_contract.sh
@@ -1,13 +1,23 @@
 set -e
 
-# Assuming a starkli account configuration file is present in $STARKNET_ACCOUNT.
-ACCOUNT_ADDRESS=$(cat $STARKNET_ACCOUNT | jq -r .deployment.address)
+# 1) To use this script, you need to have the starkli tool installed:
+# - https://book.starkli.rs/installation
+# 2) Define starkli signer. Easiest way is to use the plain text private key, however ***DO NOT DO THIS IN PRODUCTION***.
+# https://book.starkli.rs/signers
+# 3) Create an account and deploy it:
+# https://book.starkli.rs/accounts
+# Fund your newly created account either by using the faucet:
+# - Starknet faucet: https://starknet-faucet.vercel.app/
+# Or ask Itay Rosenberg to wire you some funds.
+
+# The following commands assume that you have set the environment variable as suggested in the starkli signer and account creation steps.
 
 scarb build
-CONTRACT_PATH=~/workspace/catering/packages/contracts/event_manager/target/dev/event_manager_registration.contract_class.json
+# Change the path to your contract class file. If not exist, run scarb build first.
+CONTRACT_PATH=target/dev/event_manager_registration.contract_class.json
 starkli declare $CONTRACT_PATH
 CLASS_HASH=$(starkli class-hash $CONTRACT_PATH)
-# Prevent "contract not declared" error when deploying.
+# The following command prevents "contract not declared" error when deploying.
 sleep 10
-# Deploy the contract. The $ACCOUNT_ADDRESS is the input to the contract constructor.
-starkli deploy $CLASS_HASH $ACCOUNT_ADDRESS
+# Deploy the contract. The address input is a dummy input to the constructor constructor.
+starkli deploy $CLASS_HASH 0x1234
